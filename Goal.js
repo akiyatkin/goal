b61fa1eb533fc02e425c43b96bbc9b4f7cc0e0bb
Anton <a.kiyatkin@itlf.ru>
2017-04-28 00:42:30 +0400

    handler

--- Goal.js
+++ Goal.js
@@ -2,12 +2,19 @@ window.Goal = {
 	reach: function (goal) {
 		console.log('Goal.reach '+goal);
 		if (!goal) return;
-		if (window.Ya && Ya._metrika.counter) {
+		if (window.Ya) {
 			var ya = Ya._metrika.counter;
 			ya.reachGoal(goal);
 		}
 		if (window.ga) {
 			ga('send', 'event', goal);
 		}
+	},
+	handler: function () {
+		Event.handler('Controller.onshow', function () {
+			console.log('Goal.hit');
+			if (window.ga) ga('send', 'pageview');
+			if (window.Ya) Ya._metrika.counter.hit();
+	    });
 	}
 }
