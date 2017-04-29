5f2a7f02fc055aae35c4ee3881f583dcbd3f652f
Anton <a.kiyatkin@itlf.ru>
2017-04-29 19:07:46 +0400

    fix

--- Goal.js
+++ Goal.js
@@ -20,7 +20,10 @@ window.Goal = {
 			}
 			var page = location.pathname+location.search;
 			if (window.Ya) Ya._metrika.counter.hit(page);
-			if (window.ga) ga('send', 'pageview', { page: page });
+			if (window.ga) {
+				ga('set', 'page', page);
+				ga('send', 'pageview');
+			}
 	    });
 	}
 }
