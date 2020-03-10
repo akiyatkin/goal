9470489e4b5f23a9ef7567d63686809374460a12
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-03-10 13:40:27 +0400

    fix

--- infra.js
+++ infra.js
@@ -2,6 +2,6 @@ Event.handler('Layer.onsubmit', async (layer) => {
 	if (!layer.goal) return;
 	if (!Sequence.get(layer, ['config','ans','result'])) return;
 	let Load = (await import('/vendor/akiyatkin/load/Load.js')).default
-	let Goal = await Load.on('import-default', '-goal/Goal.js')
+	let Goal = await Load.on('import-default', '/-goal/Goal.js')
 	Goal.reach(layer.goal);
 });
