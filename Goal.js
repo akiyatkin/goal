5e4e37f5300fa54ffc1413694f65d2d615a41baa
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-08 17:53:32 +0400

    fix

--- Goal.js
+++ Goal.js
@@ -1,32 +1,38 @@
-import {Wait} from "/vendor/akiyatkin/load/Wait.js"
-import {Event} from "/vendor/infrajs/event/Event.js"
-import {Once} from '/vendor/infrajs/once/Once.js'
+import { Event } from "/vendor/infrajs/event/Event.js"
+import { Once } from '/vendor/infrajs/once/Once.js'
+import { Crumb } from '/vendor/infrajs/controller/src/Crumb.js'
+import { DOM } from '/vendor/akiyatkin/load/DOM.js'
 let Goal = {
 	reach: function (goal) {
-		console.log('Goal.reach '+goal);
+		console.log('Goal.reach ' + goal);
 		if (!goal) return;
 		if (window.Ya) {
 			var ya = Ya._metrika.counter;
 			ya.reachGoal(goal);
 		}
 		if (window.gtag) {
-			gtag('event', goal,{'event_category': goal});
+			gtag('event', goal, { 'event_category': goal });
 		} else if (window.ga) {
 			ga('send', 'event', goal);
 		}
-
 		if (window.fbq) {
 			fbq('track', 'Lead');
 		}
 	},
 	ajax: async () => {
-		if (Once.omit('-goal/ajax')) return; //omit в первый раз возвращает false остальные true
-		await Wait();
-		Event.handler('Crumb.onchange', function () {
+		if (Once.omit('-goal/ajax')) return; 
+		await DOM();//Счётчик не может добавляться с async или defer
+		Event.handler('Crumb.onchange', () => {
 			if (!Once.omit('-goal/crumb')) return; //omit в первый раз возвращает false остальные true
-			var page = location.pathname+location.search+location.hash;
-			if (window.Ya) Ya._metrika.counter.hit(page);
-			
+			//Счётчики точно есть потому что это 2ой просмотр после точки входа
+			var page = location.pathname + location.search + location.hash;
+			if (window.Ya) {
+				let num = ym.a[0][0];
+				ym(num, 'hit', page, {
+					referer: Crumb.referrer
+				})
+				//Ya._metrika.counter.hit(page);
+			}
 			if (window.gtag) {
 				var trackid = dataLayer[1][1];
 				gtag('config', trackid, {
@@ -36,9 +42,9 @@ let Goal = {
 				ga('set', 'page', page);
 				ga('send', 'pageview');
 			}
-			
-	    });
+
+		});
 	}
 }
-export {Goal}
+export { Goal }
 export default Goal
--- infra.js
+++ infra.js
@@ -1,6 +1,12 @@
-Event.handler('Layer.onsubmit', async (layer) => {
+import { Fire } from '/vendor/akiyatkin/load/Fire.js'
+import { Layer } from '/vendor/infrajs/controller/src/Layer.js'
+import { Goal } from '/vendor/akiyatkin/goal/Goal.js'
+import { Seq } from '/vendor/infrajs/sequence/Seq.js'
+
+Fire.hand(Layer, 'submit', async (layer) => {
 	if (!layer.goal) return;
-	if (!Sequence.get(layer, ['config','ans','result'])) return;
-	let Goal = (await import('/vendor/akiyatkin/goal/Goal.js')).default
+	if (!Seq.getr(layer, ['config','ans','result'])) return;
 	Goal.reach(layer.goal);
 });
+
+Goal.ajax()
