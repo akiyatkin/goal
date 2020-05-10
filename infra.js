4e77dbd6f952bf73fdffa6b1203434e7d5ad9606
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-10 10:10:20 +0400

    fix

--- infra.js
+++ infra.js
@@ -1,12 +1,14 @@
-import { Fire } from '/vendor/akiyatkin/load/Fire.js'
 import { Layer } from '/vendor/infrajs/controller/src/Layer.js'
-import { Goal } from '/vendor/akiyatkin/goal/Goal.js'
+import { Event } from '/vendor/infrajs/event/Event.js'
 import { Seq } from '/vendor/infrajs/sequence/Seq.js'
 
-Fire.hand(Layer, 'submited', async (layer) => {
-	if (!layer.goal) return;
-	if (!Seq.getr(layer, ['config','ans','result'])) return;
-	Goal.reach(layer.goal);
-});
+Event.one('Controller.onshow', async () => {
+	let { Goal } = await import('/vendor/akiyatkin/goal/Goal.js')
+	Goal.ajax()
 
-Goal.ajax()
+	Layer.hand('submited', async (layer) => {	
+		if (!layer.goal) return;
+		if (!Seq.getr(layer, ['config', 'ans', 'result'])) return;
+		Goal.reach(layer.goal);
+	})
+})
