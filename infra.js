045ef1a339843ccff5c48d21436059649e7ab100
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-30 09:27:50 +0400

    fix

--- infra.js
+++ infra.js
@@ -1,13 +1,12 @@
-import { Once } from '/vendor/infrajs/once/Once.js'
-import { Event } from "/vendor/infrajs/event/Event.js"
 import { Crumb } from '/vendor/infrajs/controller/src/Crumb.js'
 
-Event.handler('Crumb.onchange', async () => {
-	if (!Once.omit('-goal/crumb')) return; //omit в первый раз возвращает false остальные true
-	//Счётчики точно есть потому что это 2ой просмотр после точки входа
-	var page = location.pathname + location.search + location.hash;
+
+Crumb.hand('change', () => {
+	if (Crumb.counter < 2) return
+	var page = location.pathname + location.search + location.hash
+	
 	if (window.Ya) {
-		let num = ym.a[0][0];
+		let num = ym.a[0][0]
 		ym(num, 'hit', page, {
 			referer: Crumb.referrer
 		})
@@ -19,8 +18,7 @@ Event.handler('Crumb.onchange', async () => {
 			'page_path': page
 		});
 	} else if (window.ga) {
-		ga('set', 'page', page);
-		ga('send', 'pageview');
+		ga('set', 'page', page)
+		ga('send', 'pageview')
 	}
-
-});
+})
