506b706a4b7be4d23ad77fd162ab8d0198f9c95e
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-11-15 20:52:31 +0400

    fix ignore host

--- infra.js
+++ infra.js
@@ -6,11 +6,15 @@ Crumb.hand('change', () => {
 	var page = location.pathname + location.search + location.hash
 	
 	if (window.Ya) {
-		let num = ym.a[0][0]
-		ym(num, 'hit', page, {
-			referer: Crumb.referrer
-		})
-		//Ya._metrika.counter.hit(page);
+		if (window.ym) {
+			let num = ym.a[0][0]
+			ym(num, 'hit', page, {
+				referer: Crumb.referrer
+			})
+		} else if (Ya._metrika.counter) {
+			Ya._metrika.counter.hit(page);	
+		}
+		
 	}
 	if (window.gtag) {
 		var trackid = dataLayer[1][1];
