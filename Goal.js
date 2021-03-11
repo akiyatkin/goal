f53ad9a1305fc7ffaf03290b7a72d38a48963690
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2021-03-11 14:56:44 +0400

    fix

--- Goal.js
+++ Goal.js
@@ -5,18 +5,26 @@ let Goal = { ...Fire,
 	reach: function (goal) {
 		console.log('Goal.reach ' + goal);
 		if (!goal) return;
-		if (window.Ya) {
-			var ya = Ya._metrika.counter;
-			ya.reachGoal(goal);
+
+		if (window.dataLayer && dataLayer[0] && dataLayer[0]["gtm.start"]) {
+			dataLayer.push({
+				'event': goal
+			});
+		} else {
+			if (window.Ya) {
+				const ya = Ya._metrika.counter;
+				ya.reachGoal(goal);
+			}
+			if (window.gtag) {
+				gtag('event', goal, { 'event_category': goal });
+			} else if (window.ga) {
+				ga('send', 'event', goal);
+			}
+			if (window.fbq) {
+				fbq('track', 'Lead');
+			}	
 		}
-		if (window.gtag) {
-			gtag('event', goal, { 'event_category': goal });
-		} else if (window.ga) {
-			ga('send', 'event', goal);
-		}
-		if (window.fbq) {
-			fbq('track', 'Lead');
-		}
+		
 	}
 }
 
--- infra.js
+++ infra.js
@@ -1,28 +1,40 @@
 import { Crumb } from '/vendor/infrajs/controller/src/Crumb.js'
+import { Controller } from '/vendor/infrajs/controller/src/Controller.js'
 
 
-Crumb.hand('change', () => {
+Crumb.done('change', async () => {
 	if (Crumb.counter < 2) return
-	var page = location.pathname + location.search + location.hash
+	if (Controller.check.promise) await Controller.check.promise
+	const page = location.href; // location.pathname + location.search + location.hash
+	const title = document.title
 	
-	if (window.Ya) {
-		if (window.ym) {
-			let num = ym.a[0][0]
-			ym(num, 'hit', page, {
-				referer: Crumb.referrer
-			})
-		} else if (Ya._metrika.counter) {
-			Ya._metrika.counter.hit(page);	
-		}
-		
-	}
-	if (window.gtag) {
-		var trackid = dataLayer[1][1];
-		gtag('config', trackid, {
-			'page_path': page
+	if (window.dataLayer && dataLayer[0] && dataLayer[0]["gtm.start"]) {
+		// В GA4 не должно быть галочки в page_view в автоматических событиях
+		// Метрика никак не обрабатывает history только через это специально событие
+		dataLayer.push({
+			'event': 'pageview'
 		});
-	} else if (window.ga) {
-		ga('set', 'page', page)
-		ga('send', 'pageview')
+	} else {// Добавлено 11 марта 2021 года в 11:59
+		if (window.Ya) {
+			if (window.ym) {
+				let num = ym.a[0][0]
+				ym(num, 'hit', page, {
+					referer: Crumb.referrer
+				})
+			} else if (Ya._metrika.counter) {
+				Ya._metrika.counter.hit(page);
+			}
+			
+		}
+		if (window.gtag) {
+			var trackid = dataLayer[1][1];
+			gtag('config', trackid, {
+				'page_path': page
+			});
+		} else if (window.ga) {
+			ga('set', 'page', page)
+			ga('send', 'pageview')
+		}
 	}
+	
 })
