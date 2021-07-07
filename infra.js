64d80689272b8a8fda25edcd6b1aa9da915f9394
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2021-07-07 14:58:01 +0400

    fix

--- infra.js
+++ infra.js
@@ -18,33 +18,44 @@ DOM.done('load', () => {
 Crumb.done('change', async () => {
 	if (Crumb.counter < 2) return
 	if (Controller.check.promise) await Controller.check.promise
-	const page = location.href; // location.pathname + location.search + location.hash
-	const title = document.title
+	//const page = location.href; // location.pathname + location.search + location.hash
+	//const title = document.title
 	
 
 	//Событие асинхронного перехода всегда отправляем с сайта.
 	if (window.Ya) { //Неважно как установлена метрика. 
-		if (window.ym) {
-			let num = ym.a[0][0]
-			ym(num, 'hit', page, {
-				referer: Crumb.referrer
-			})
-		} else if (Ya._metrika.counter) {
-			Ya._metrika.counter.hit(page);
-		}
+		// if (window.ym) {
+		// 	let num = ym.a[0][0]
+		// 	ym(num, 'hit', page, {
+		// 		referer: Crumb.referrer
+		// 	})
+		// } else if (Ya._metrika.counter) {
+			Ya._metrika.counter.hit(location.href);
+			//Ya._metrika.counter.reachGoal('pageview');
+		//}
 		
 	}
 	if (window.gtag) { //Неважно как установлен GA и какой версии. Шлём события pageview. 
 		var trackid = dataLayer[1][1];
 		gtag('config', trackid, {
-			'page_path': page
+			'page_title' : document.title,
+			'page_path': location.href
 		});
 	} else if (window.ga) {
-		ga('set', 'page', page)
+		ga('set', 'page', location.href)
+		ga('set', 'title', document.title)
 		ga('send', 'pageview')
 	}
 
-
+	if (window.dataLayer) {
+		dataLayer.push({
+			event: 'pageview',
+			page: {
+				path: location.href,
+				title: document.title
+			}
+		});
+	}
 
 	// if (window.dataLayer && dataLayer[0] && dataLayer[0]["gtm.start"]) {
 	// 	// В GA4 не должно быть галочки в page_view в автоматических событиях
