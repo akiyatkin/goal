23730e8f478daf178952bedbf84598616eaf0533
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2021-07-07 14:03:59 +0400

    fix

--- Goal.js
+++ Goal.js
@@ -1,32 +1,43 @@
 import { Seq } from '/vendor/infrajs/sequence/Seq.js'
 import { Fire } from '/vendor/akiyatkin/load/Fire.js'
 
-let Goal = { ...Fire,
+const Goal = { ...Fire,
 	reach: function (goal) {
 		console.log('Goal.reach ' + goal);
 		if (!goal) return;
+		
+		// if (window.dataLayer && dataLayer[0] && dataLayer[0]["gtm.start"]) {
+		// 	//Такое событие попадёт и в GA4 и в GU
+		// 	dataLayer.push({
+		// 		'event': goal
+		// 	});
+		// } else {
+		// 	if (window.gtag) {
+		// 		gtag('event', goal, { 'event_category': goal });
+		// 	}
+		// }
+		// if (window.ga) {
+		// 	ga('send', 'event', goal);
+		// }
 
-		if (window.dataLayer && dataLayer[0] && dataLayer[0]["gtm.start"]) {
+		if (window.dataLayer) {
 			dataLayer.push({
 				'event': goal
 			});
 		} else {
-			if (window.Ya) {
-				const ya = Ya._metrika.counter;
-				ya.reachGoal(goal);
-			}
-			if (window.gtag) {
-				gtag('event', goal, { 'event_category': goal });
-			} else if (window.ga) {
+			if (window.ga) {
 				ga('send', 'event', goal);
 			}
 			if (window.fbq) {
 				fbq('track', 'Lead');
 			}	
+			if (window.Ya) {
+				const ya = Ya._metrika.counter;
+				ya.reachGoal(goal);
+			}
 		}
-		
 	}
 }
-
+window.Goal = Goal
 export { Goal }
 export default Goal
--- infra.js
+++ infra.js
@@ -21,33 +21,40 @@ Crumb.done('change', async () => {
 	const page = location.href; // location.pathname + location.search + location.hash
 	const title = document.title
 	
-	if (window.dataLayer && dataLayer[0] && dataLayer[0]["gtm.start"]) {
-		// В GA4 не должно быть галочки в page_view в автоматических событиях
-		// Метрика никак не обрабатывает history только через это специально событие
-		dataLayer.push({
-			'event': 'pageview'
-		});
-	} else {// Добавлено 11 марта 2021 года в 11:59
-		if (window.Ya) {
-			if (window.ym) {
-				let num = ym.a[0][0]
-				ym(num, 'hit', page, {
-					referer: Crumb.referrer
-				})
-			} else if (Ya._metrika.counter) {
-				Ya._metrika.counter.hit(page);
-			}
-			
+
+	//Событие асинхронного перехода всегда отправляем с сайта.
+	if (window.Ya) { //Неважно как установлена метрика. 
+		if (window.ym) {
+			let num = ym.a[0][0]
+			ym(num, 'hit', page, {
+				referer: Crumb.referrer
+			})
+		} else if (Ya._metrika.counter) {
+			Ya._metrika.counter.hit(page);
 		}
-		if (window.gtag) {
-			var trackid = dataLayer[1][1];
-			gtag('config', trackid, {
-				'page_path': page
-			});
-		} else if (window.ga) {
-			ga('set', 'page', page)
-			ga('send', 'pageview')
-		}
+		
 	}
+	if (window.gtag) { //Неважно как установлен GA и какой версии. Шлём события pageview. 
+		var trackid = dataLayer[1][1];
+		gtag('config', trackid, {
+			'page_path': page
+		});
+	} else if (window.ga) {
+		ga('set', 'page', page)
+		ga('send', 'pageview')
+	}
+
+
+
+	// if (window.dataLayer && dataLayer[0] && dataLayer[0]["gtm.start"]) {
+	// 	// В GA4 не должно быть галочки в page_view в автоматических событиях
+	// 	// Метрика никак не обрабатывает history только через это специально событие
+	// 	// dataLayer.push({
+	// 	// 	'event': 'pageview'
+	// 	// });
+	// } else { // Добавлено 11 марта 2021 года в 11:59
+		
+		
+	// }
 	
 })
